efd1be9864977a695bb0ff760106ed653d9c46a0
Christopher Hudson <c.ahudson84@yahoo.com>
2017-03-13 14:34:47 -0700

    Update

--- ex36.py
+++ ex36.py
@@ -22,6 +22,24 @@ game_state = {
     inventory = None
 }
 
+input_dict ={
+
+}
+
+
+class Item(object):
+    def __init__(self, name, description, take_me):
+        self.name = name
+        self.description = description
+        self.take_me = take_me
+        self.valid_choices = valid_choices
+
+def item_string_convert(t_choice):
+    for Item in input_dict.keys():
+        if t_choice in input_dict[key].valid_choices:
+            return input_dict[key]
+
+
 def do_loop():
     while do_loop = True:
         choice = raw_input("What do you choose to do?\n> ")
@@ -64,61 +82,47 @@ def do_loop():
             else:
                 print "That is not helpful! next time try looking at something in the room or just go back..."
 
-
-        elif choice in take
+        elif choice in take:
             t_choice = raw_input("What would you like to take?\n> ")
+
                 if start_item = True
                     print "You have chosen your path! Only one may be taken, move along!"
                     do_loop()
 
-                elif t_choice = item1
-                    if item1 in game_state.inventory()
-                        print "You already have that."
-                        do_loop()
-                    else:
-                        print "You took %s, %s!" % item1, item1des
-                        do_loop()
-                        game_state.inventory.append(item1)
+                elif item in master_items_dict and item not in game_state.inventory:
+                    game_state.inventory.append(item)
+                    master_items_dict.remove(item)
+                    print(item.take_me)
+                elif item in game_state.inventory:
+                    print ("You already have that.")
 
-                elif t_choice = item2
-                    if item2 in game_state.inventory()
-                        print "You already have that."
-                        do_loop()
-                    else:
-                        print "You took %s, %s!" % item2, item2des
-                        do_loop()
-                        game_state.inventoy.append(item2)
+            else:
+                print "You can't take that try something else, maybe try 'the item'."
+                do_loop()
 
-                elif t_choice = item3
-                    if item3 in game_state.inventory()
-                        print "You already have that."
-                        do_loop()
-                    else:
-                        print "You took %s, %s!" % item3, item3des
-                        do_loop()
-                        game_state.inventory.append(item3)
+
+                if start_item = True
+                    print "You have chosen your path! Only one may be taken, move along!"
+                    do_loop()
 
                 elif t_choice in back
                     print "Guess you don't want any of this junk, back to it."
                     do_loop()
 
-                else:
-                    print "You can't take that try something else, maybe try 'the item'."
-                    do_loop()
 
 
-        elif choice == lis
+        elif choice in lis
             li_choice = raw_input("What would you like to listen to?\n> ")
 
-        elif choice == ent
+        elif choice in ent
             e_choice = raw_input("Where would you like to enter?\n> ")
 
-        elif choice == help
+        elif choice in help
             print "Things you can do: "
             print "Look, Take, Listen, Enter."
             print "These can be used with objects as well such as doors!"
 
-        elif choice == die
+        elif choice in die
             if dragon_in_room:
                 print "You decide to die here but there is a dragon, it toasts you alive then eats you in two bites"
             else:
@@ -154,26 +158,46 @@ def entrance_hall():
     print "If you would like to see what you have use Inventory or I."
     print "Use back to make a different choice."
 
-if game_state.inventoy(None) = False
-    start_item = True
+    if game_state.inventoy(None) = False
+        start_item = True
 
     doors = ("A Black door", "A Red door", "A Silver door")
     items = ("A Staff", "A Sword", "A Cloak")
-    item1 = ("the staff")
-    item1des = ("the torches blaze up, wind howles through the room, and lightning strikes the crystal atop it")
-    item2 = ("the sword")
-    item2des = ("")
-    item3 = ("the cloak")
-    item3des = ("")
+
+    staff = Item(
+    name = "The Staff of Power"
+    description = "The Staff is tall and twisted, made of a deep dark wood and topped with an ever changing crystal.",
+                    " It exudes power, pulsing and searing against the air."
+    take_me = "The torches blaze up, wind howles through the room, and lightning strikes the crystal atop it! You got The Staff of Power!"
+    valid_choices = ("Staff", "staff", "A Staff", "a Staff", "A staff", "a staff", "The Staff",
+    "the Staff", "The staff", "the staff")
+    )
+    input_dict.append(staff)
+
+    sword = Item(
+    name = "The "
+    description = "The Sword double edged and roughly three feet long but oddly light, it has intricit etchings on either side of the blades face. ",
+    " Even with the fine detail their isn't a single blemish on it, you get the feeling you couldn't break it if you tried."
+    take_me = ""
+    valid_choices = ("Sword", "sword", "A Sword", "a Sword", "A sword", "a sword", "The Sword",
+    "the Sword", "The sword", "the sword")
+    )
+    input_dict.append(sword)
+
+    cloak = Item(
+    name = "Nigh"
+    description = "The Cloak is cool and warm, black and shimmering and all colors at once. At times you can't even really see it.",
+    "It is a bit unnearving while also being calming, almost protective."
+    take_me =
+    valid_choices = ("Cloak", "cloak", "A Cloak", "a Cloak", "A cloak", "a cloak", "The Cloak",
+    "the Cloak", "The cloak", "the cloak")
+    )
+    input_dict.append(cloak)
+
     closerlookdoors = ("The Black door is so dark you have a hard time telling it is even there.",
                     "The Red door appears as if it is on fire and is even a little warm to the touch.",
                     "The Silver door is made of metal and is slightly cool to the touch.")
-    closerlookitems = ("The Staff is tall and twisted, made of a deep dark wood and topped with an ever changing crystal.",
-                    " It exudes power, pulsing and searing against the air.",
-                    "\nThe Sword double edged and roughly three feet long but oddly light, it has intricit etchings on either side of the blades face. ",
-                    " Even with the fine detail their isn't a single blemish on it, you get the feeling you couldn't break it if you tried.",
-                    "\nThe Cloak is cool and warm, black and shimmering and all colors at once. At times you can't even really see it.",
-                    "It is a bit unnearving while also being calming, almost protective.")
+    closerlookitems = (staff.description, sword.description, cloak.description)
     roomlis = "Listening to the room you hear the torches crackling and a subtle thruming, as if the very air is vibrating."
     doorlis = (door1, door2, door3)
     door1 = ("Listening at the Black door you hear only the slightest, quietest breath of wind and an unsettling amount of nothing else.")
