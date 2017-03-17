7bf5aaf4ea7ccfe49b7fb13e0408407d42ff8b6b
Christopher Hudson <c.ahudson84@yahoo.com>
2017-03-17 11:09:47 -0700

    Update

--- ex36.py
+++ ex36.py
@@ -10,22 +10,24 @@ ent = ("Enter", "enter")
 door = ("Door", "door", "the door", "The door","the Door", "The Door",
 "Doors", "doors", "the doors", "The doors","the Doors", "The Doors",)
 item = ("Item, item, Items, items")
+help = ("Help", "help", "H", "h")
 back = ("Back, back, B, b")
 yes = ("yes", "y")
 no = ("no", "n")
 die = ("die", "Die", "I die", "I Die", "i die", "i Die", "dead", "Dead")
 inventory = ("Inventory", "inventory", "I", "i")
+quit = ("Quit", "quit", "Q", "q", "Exit", "exit", "E", "e")
 
 dragon_in_room = False
 
 game_state = {
-    "inventory": None
+    "inventory": []
+    "start item": []
 }
 
 input_dict ={
-    "keys": None
+    "keys": []
 }
-
 
 class Item(object):
     def __init__(self, name, description, take_me):
@@ -37,9 +39,8 @@ class Item(object):
 def pickup_converter(t_choice):
     for key in input_dict.keys():
         if t_choice in input_dict[key].valid_choices:
-            return input_dict[key]
 
-            if start_item is True:
+            if game_state.start_item:
                 print "You have chosen your path! Only one may be taken, move along!"
                 do_loop()
 
@@ -115,10 +116,16 @@ def do_loop():
             e_choice = raw_input("Where would you like to enter?\n> ").lower()
                 if e_choice in door1:
                     return ldoor
+                    do_loop()
+
                 elif e_choice in door2:
                     return cdoor
+                    do_loop()
+
                 elif echoice in door3:
                     return rdoor
+                    do_loop()
+
                 else:
                     print "Try choosing a door next time?"
                     do_loop()
@@ -126,7 +133,10 @@ def do_loop():
         elif choice in help:
             print "Things you can do: "
             print "Look, Take, Listen, Enter."
-            print "These can be used with objects as well such as doors!"
+            print "These can be used with objects or doors!"
+            print "Other commands include: "
+            print "Yes, No, Back, Quit, and Die."
+            do_loop()
 
         elif choice in die:
             if dragon_in_room:
@@ -136,6 +146,7 @@ def do_loop():
                 print "GAME OVER!"
                     restart = raw_input("Would you like to Restart?\n> ")
                     if restart in yes:
+                        game_state["inventoy"] = []
                         entrance_hall()
                     elif restart in no:
                         exit(0)
@@ -143,8 +154,24 @@ def do_loop():
                         print "I will take that as a no."
                         exit(0)
 
+        elif choice in quit:
+            exit(0)
+
         else:
             print "That is not very helpful. Looking around or something or ask for help?"
+            do_loop()
+
+def dead(why):
+    print why, "You have died!"
+    print "GAME OVER!"
+        restart = raw_input("Would you like to Restart?\n> ")
+        if restart in yes:
+            entrance_hall()
+        elif restart in no:
+            exit(0)
+        else:
+            print "I will take that as a no."
+            exit(0)
 
 def entrance_hall():
     print "You wake up to find yourself in a large chamber lit by torches."
@@ -163,11 +190,6 @@ def entrance_hall():
     print "Use look to look at things(usually an item(s) or door(s))."
     print "If you would like to see what you have use Inventory or I."
     print "Use back to make a different choice."
-
-
-
-    if game_state.inventoy(None) False:
-        start_item = True
 
     doors = ("A Black door", "A Red door", "A Silver door")
     door1 = ("black door", "the black door", "a black door")
@@ -211,11 +233,17 @@ def entrance_hall():
     closerlookdoors = ("The Black door is so dark you have a hard time telling it is even there.",
                     "The Red door appears as if it is on fire and is even a little warm to the touch.",
                     "The Silver door is made of metal and is slightly cool to the touch.")
+
     closerlookitems = (staff.description, sword.description, cloak.description)
+
     roomlis = "Listening to the room you hear the torches crackling and a subtle thruming, as if the very air is vibrating."
+
     doorlis = (door1, door2, door3)
+
     door1 = ("Listening at the Black door you hear only the slightest, quietest breath of wind and an unsettling amount of nothing else.")
+
     door2 = ("Listening at the Red door you hear the crackle of fire, as if from more torches and a deep distant rumble.")
+
     door3 = ("Listening at the Silver door you hear running water, an odd snorting, and a very faint, very distant mixture of rumbling")
 
     do_loop()
@@ -228,7 +256,12 @@ def goblin_room():
 
 def lava_room():
 
-
+def infinite_room():
+    print "You find yourself in a black room."
+    print "It is devoid of all color, life or light. It is not dark simply black."
+    print "Even so you can see a door opposite you and the door that has just swung shut behind you."
+    print "There is nothing here in fact the room is it self nothing."
+    do_loop()
 
 def dragon_chamber():
     dragon = True
